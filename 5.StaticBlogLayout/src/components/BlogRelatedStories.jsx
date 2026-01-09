c120853296305192a47a3a69de12ee9cc5104cf7
Josh Bello <Jb230913@live.com>
2026-01-09 19:04:23 +0000

    add - BlogRelatedStories html elements with utilities class

---
+++ 5.StaticBlogLayout/src/components/BlogRelatedStories.jsx
@@ -0,0 +1,46 @@
+function BlogRelatedStories() {
+
+    return <section className="" data-block="our_stories_main_image" data-template="view">
+        <div className="container">
+            <div className="row">
+                <div className="col col-sm-3 col-md-3">
+                    <div className="related_stories_bloc">
+                        <img className="img-fluid" src="https://loremflickr.com/250/250" alt="" />
+                        <h2>Lorem ipsum dolor sit.</h2>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat et asperiores distincti....</p>
+
+                        <span>By Author X</span>
+                    </div>
+
+
+                </div>
+                <div className="col col-sm-3 col-md-3">
+                    <div className="related_stories_bloc">
+                        <img className="img-fluid" src="https://loremflickr.com/250/250" alt="" />
+                        <h2>Lorem ipsum dolor sit.</h2>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat et asperiores distincti....</p>
+
+                        <span>By Author X</span>
+                    </div>
+
+
+                </div>
+                <div className="col col-sm-3 col-md-3">
+                    <div className="related_stories_bloc">
+                        <img className="img-fluid" src="https://loremflickr.com/250/250" alt="" />
+                        <h2>Lorem ipsum dolor sit.</h2>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat et asperiores distincti....</p>
+
+                        <span>By Author X</span>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+}
+
+
+export default BlogRelatedStories;
