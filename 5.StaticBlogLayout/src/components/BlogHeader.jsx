710a6a0301655d7c1a111de6b404524ecab65ee8
Josh Bello <Jb230913@live.com>
2026-01-09 18:21:30 +0000

    update - added bootstrap utilities class

--- 5.StaticBlogLayout/src/components/BlogHeader.jsx
+++ 5.StaticBlogLayout/src/components/BlogHeader.jsx
@@ -2,14 +2,14 @@
 function BlogHeader() {
 
     return <article className="hidden d-none d-sm-block md.block py-24 m" data-block="name-here" data-template="view">
-        <div className="l-container">
-            <div className="row | d-flex justify-content-start">
-                <div className="col col-sm-12 col-md-8  | mb-auto">
-                    <a href="#">Go Back</a>
+        <div className="container">
+            <div className="row | d-flex justify-content-md-start">
+                <div className="col col-sm-12 col-md-8  | mb-auto | text-start">
+                    <a className="" href="#">Go Back</a>
                 </div>
                 <div className="col col-sm-12 col-md-2 offset-md-2  ">
                     <label className="d-flex   | mb-12" for="topics">Date</label>
-                    <ul className="d-flex flex-row flex-md-column  ">
+                    <ul className=" d-flex flex-row flex-md-column  p-0 | list-unstyled">
                         <li>#Category 1</li>
                         <li>#Category 2</li>
                         <li>#Category 3</li>
@@ -20,7 +20,7 @@ function BlogHeader() {
 
             <div className="row">
 
-                <div className="col col-md-8">
+                <div className="col col-md-8 | text-start">
                     <h1>Lorum ipsum dolor sit amet...</h1>
 
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna...</p>
@@ -28,7 +28,7 @@ function BlogHeader() {
                 <div className="col col-md-2 offset-md-2 ">
                     <figure className="d-flex | border-b-2 border-gray-300 mb-16 pb-24">
                         <img src="https://placeholder.pics/svg/160x160" alt="pic of Author" loading="lazy" />
-                        <figcaption className="ml-12 | d-flex flex-column">
+                        <figcaption className="ml-12 | d-flex flex-column | text-start">
                             <strong>Amanda Faull </strong>
                             Diversity, Equity & Inclusion Communications Specialist
                         </figcaption>
