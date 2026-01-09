5655f48c0ffe403828efe6a990623b18183fc103
Josh Bello <Jb230913@live.com>
2026-01-09 18:27:42 +0000

    add - BlogMainPicture html elements with utilities class

---
+++ 5.StaticBlogLayout/src/components/BlogMainPicture.jsx
@@ -0,0 +1,21 @@
+
+
+function BlogMainPicture() {
+
+    return <section className="" data-block="our_stories_main_image" data-template="view">
+        <div className="container">
+            <div className="row | d-flex ">
+                <div className="col-xs-12 col-sm-12 col-md-10 col-lg-10 ">
+                    <figure className="">
+                        <img className="img-fluid" src="https://loremflickr.com/736/479" alt="" />
+                        <figcaption>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, id!</figcaption>
+                    </figure>
+
+                </div>
+            </div>
+        </div>
+    </section >
+}
+
+
+export default BlogMainPicture;
